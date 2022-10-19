fe8f3c0b6f129f6fff04ee7b48af3bfbcfd6dd5c
jfpanchi2 <jfpanchi2@espe.edu.ec>
2022-10-18 19:33:50 -0500

    Documentacion Endpoint Listar Pokemones
    Se documento utilizando swagger el Endpoint Listar Pokemones

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
+const path = require("path");
 const express = require('express');
 const { dbConnection } = require('./database/config');
+const swaggerUI = require('swagger-ui-express');
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerSpec = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "Pokemon Api Challenge",
+            version: "1.0.0"
+        },
+        servers: [
+            {
+                url: "http://localhost:4000"
+            }
+        ]
+    },
+    apis: [`${path.join(__dirname, "./routes/*.js")}`]
+}
 require('dotenv').config();
 const app = express();
 
@@ -8,10 +26,14 @@ dbConnection();
 
 //Directorio público
 app.use(express.static('public'));
+// Lectura y parse
+app.use(express.json());
 
 //Rutas
 // POKEMONES: listar
 app.use('/api/pokemones',require('./routes/pokemones'));
+// DOCUMENTACIÓN: swagger
+app.use('/api/doc',swaggerUI.serve, swaggerUI.setup(swaggerJsDoc(swaggerSpec)));
 
 //Escuchar peticiones 
 app.listen(process.env.PORT,()=>{
--- routes/pokemones.js
+++ routes/pokemones.js
@@ -1,12 +1,57 @@
-/*  
-    Rutas de los pokemones /pokemones
-    host = /api/pokemones
-*/
+
 const {Router} = require('express');
 const { listarPokemones } = require('../controllers/pokemones');
 const router = Router();
+//Pokemon
+/**
+ * @swagger
+ * components: 
+ *  schemas:
+ *      Pokemon:
+ *        type: object
+ *        properties:
+ *          id:
+ *           type: string
+ *           description: identificador Pokemon PokeApi
+ *          nombre:
+ *           type: string
+ *           description: Nombre del Pokemon
+ *          descripcion:
+ *           type: string
+ *           description: descripcion del Pokemon
+ *          tipo:
+ *           type: string
+ *           description: tipo del Pokemon
+ *          evolucion:
+ *           type: Array
+ *           description: Lista de nombres de la evoluciòn del Pokemon
+ *          altura:
+ *           type: number
+ *           description: Altura del Pokemon
+ *          peso:
+ *           type: number
+ *           description: Peso del Pokemon
+ *          imagen:
+ *           type: number
+ *           description: Imagen del Pokemon 
+ *        required:
+ *          - id
+ *          - nombre
+ *          - descripcion
+ *             
+ */
 
-//Obtener Usuarios
+//Listar Pokemones
+/**
+ * @swagger
+ * /api/pokemones:
+ *  get:
+ *      summary: Lista los Pokemones
+ *      tags: [Pokemon]
+ *      responses: 
+ *          200: 
+ *              description: Listar Pokemones      
+ */
 router.get('/', listarPokemones);
 
 module.exports = router;
